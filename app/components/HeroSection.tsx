2f40da14e77b5e8cfa3a2a8fdcb414b4237e07f1
supriyaarora12 <supriya.arora.hp@gmail.com>
2025-08-22 19:32:38 +0530

    deploy/6

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import HeroCarousel from './HeroCarousel';
 export default function HeroSection() {
   return (
     <section className=" container px-4 pb-8">
-      <div className="pl-[95px] pr-[117px] flex flex-col lg:flex-row gap-4 lg:gap-6">
+      <div className="pl-[95px] pr-[160px] flex flex-col lg:flex-row gap-4 lg:gap-6">
         
     {/* Left Menu - Now always visible */}
     <div>
@@ -12,7 +12,7 @@ export default function HeroSection() {
     </div>  
     
         {/* Hero Carousel - Full width on mobile, flex-1 on desktop */}
-        <div className="w-full lg:flex-1 pt-0 lg:pt-4">
+        <div className="w-full lg:flex-1 pt-0 mr-[10px] lg:pt-4">
           <HeroCarousel />
         </div>
         
